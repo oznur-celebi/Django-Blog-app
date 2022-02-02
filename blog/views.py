a4e918f8778f99de3a93524abc5382f9d4788564
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-02 23:19:34 +0100

    model.py added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.template import context
+from .models import Post
 # from django.http import HttpResponse
 
 # def home(request):
@@ -8,24 +9,24 @@ from django.template import context
 # def about(request):
 #     return HttpResponse('<h1>Blog About</h1>')
 
-posts =[
-    {
-        'author':'CoreyMs',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'August 27, 2018'
+# posts =[
+#     {
+#         'author':'CoreyMs',
+#         'title':'Blog Post 1',
+#         'content':'First post content',
+#         'date_posted':'August 27, 2018'
 
-    },
-    {
-        'author':'Janet Doe',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'August 28, 2018'
-    }
-]
+#     },
+#     {
+#         'author':'Janet Doe',
+#         'title':'Blog Post 2',
+#         'content':'Second post content',
+#         'date_posted':'August 28, 2018'
+#     }
+# ]
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return  render(request, 'blog/home.html',context)
 
