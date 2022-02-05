c2cc4f6451fe9f22e65cfe7ffc2dffc5d0ec02ab
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-06 00:41:34 +0100

    settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY ="django-insecure-!8^gc0fkb9fa9k1ed_a-+vq_+))lp!k+3y4a0^e*ictz2zum0v"
-SECRET_KEY =config('SECRET_KEY')
+SECRET_KEY =config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
