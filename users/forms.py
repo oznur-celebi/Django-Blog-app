005b9be9e08db5aa64ac2eec1be893e67df986de
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-05 14:15:06 +0100

    forms.py added

---
+++ users/forms.py
@@ -0,0 +1,14 @@
+from dataclasses import fields
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    firstname =forms.CharField(max_length=20)
+    surname =forms.CharField(max_length=20)
+
+
+    class Meta:
+        model =User
+        fields = ['username', 'firstname' , 'surname','email', 'password1', 'password2']
