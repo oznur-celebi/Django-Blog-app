2f7f467c4d18222edda337216814192f1e032756
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-06 01:44:20 +0100

    logout.html added

--- users/forms.py
+++ users/forms.py
@@ -5,10 +5,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    firstname =forms.CharField(max_length=20)
-    surname =forms.CharField(max_length=20)
+    first_name =forms.CharField(max_length=50)
+    last_name =forms.CharField(max_length=50)
 
 
     class Meta:
         model =User
-        fields = ['username', 'firstname', 'surname','email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name','email', 'password1', 'password2']
