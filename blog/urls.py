144143e2da6f2322cc5dabfc011a0c602565ce97
Oznur Celebi <o.ozgumus17@gmail.com>
2022-02-08 23:50:48 +0100

    PostListView and PostDetailView are created

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import PostListView,PostDetailView
 from .views import home, about
 
 urlpatterns = [
-    path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('about/', about, name='blog-about'),
 ]
